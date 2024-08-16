88281aa4379d2764975bfc4916f2adbe194157a7
cletus <cletushunsu@gmail.com>
2024-08-16 04:24:35 -0700

    created Teacher model, linking student model as a one-to-many relationship

---
+++ SchoolManagementAPI/Models/Teacher.cs
@@ -0,0 +1,12 @@
+﻿// Teacher model 
+namespace SchoolManagementAPI.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        // one-to-many relationship
+        public ICollection<Student>? Students { get; set; }
+    }
+}
