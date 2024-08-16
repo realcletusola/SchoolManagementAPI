94760311d4f5278bb3979174c7fd9038e2f0fc6f
cletus <cletushunsu@gmail.com>
2024-08-16 04:21:45 -0700

    created student profile model, linking student model as a foreign key element

---
+++ SchoolManagementAPI/Models/StudentProfile.cs
@@ -0,0 +1,14 @@
+﻿// student profile model 
+namespace SchoolManagementAPI.Models
+{
+    public class StudentProfile
+    {
+        public int StudentProfileId { get; set; } // primary Key 
+        public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        // Foreign Key for student (one-to-one Relationship)
+        public int StudentId { get; set; }
+        public Student? Student { get; set; } // Navigation Property 
+    }
+}
