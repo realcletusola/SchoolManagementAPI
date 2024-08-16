119303d92e38e3e011f01b10dee6371e699cb26d
cletus <cletushunsu@gmail.com>
2024-08-16 07:16:19 -0700

    created teachers controller

---
+++ SchoolManagementAPI/Controllers/TeachersController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagementAPI.Data;
+using SchoolManagementAPI.Models;
+
+
+namespace SchoolManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeachersController : ControllerBase
+    {
+        private readonly SchoolManagementContext _context;
+
+        public TeachersController(SchoolManagementContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Teachers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Teacher>>> GetTeachers()
+        {
+            return await _context.Teachers.ToListAsync();
+        }
+
+        // GET: api/Teachers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Teacher>> GetTeacher(int id)
+        {
+            var teacher = await _context.Teachers.FindAsync(id);
+
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            return teacher;
+        }
+
+        // POST: api/Teachers
+        [HttpPost]
+        public async Task<ActionResult<Teacher>> PostTeacher(Teacher teacher)
+        {
+            _context.Teachers.Add(teacher);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTeacher), new { id = teacher.TeacherId }, teacher);
+        }
+
+        // PUT: api/Teachers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTeacher(int id, Teacher teacher)
+        {
+            if (id != teacher.TeacherId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(teacher).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeacherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // PATCH: api/Teachers/5
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> PatchTeacher(int id, [FromBody] JsonPatchDocument<Teacher> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest("Patch document is null.");
+            }
+
+            var teacher = await _context.Teachers.FindAsync(id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            patchDoc.ApplyTo(teacher, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeacherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Teachers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTeacher(int id)
+        {
+            var teacher = await _context.Teachers.FindAsync(id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            _context.Teachers.Remove(teacher);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TeacherExists(int id)
+        {
+            return _context.Teachers.Any(e => e.TeacherId == id);
+        }
+    }
+}
