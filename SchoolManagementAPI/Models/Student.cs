46d471b4096c5ef80910e108a4aeb465d112671d
cletus <cletushunsu@gmail.com>
2024-08-16 04:17:54 -0700

    created student model

--- SchoolManagementAPI/Models/Student.cs
+++ SchoolManagementAPI/Models/Student.cs
@@ -3,5 +3,7 @@ namespace SchoolManagementAPI.Models
 {
     public class Student
     {
+        public int StudentId { get; set; } // primary Key
+        public string Name { get; set; } = string.Empty;
     }
 }
