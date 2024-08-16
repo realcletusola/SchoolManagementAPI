06aa1c5efd0912ead3388750fdb88433d4c36f24
cletus <cletushunsu@gmail.com>
2024-08-16 04:37:17 -0700

    created enrollment model

---
+++ SchoolManagementAPI/Models/Enrollment.cs
@@ -0,0 +1,12 @@
+﻿// enrollment model 
+namespace SchoolManagementAPI.Models
+{
+    public class Enrollment
+    {
+        public int StudentId { get; set; } // Foreign key to student model 
+        public Student? Student { get; set; } // Navigation property 
+
+        public int CourseId { get; set; } // Foriegn Key to course 
+        public Course? Course { get; set; } // Navigation Property 
+    }
+}
