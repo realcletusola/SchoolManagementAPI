3e3ad4ad62c27f1b3806e2a078a3af0bf6ff32ff
cletus <cletushunsu@gmail.com>
2024-08-16 04:35:41 -0700

    Added foreign key relationship for Teacher, StudentProfile, and Enrollment

--- SchoolManagementAPI/Models/Student.cs
+++ SchoolManagementAPI/Models/Student.cs
@@ -5,5 +5,15 @@ namespace SchoolManagementAPI.Models
     {
         public int StudentId { get; set; } // primary Key
         public string Name { get; set; } = string.Empty;
+
+        // Foreign Key for Teacher 
+        public int TeacherId { get; set; }
+        public Teacher? Teacher { get; set; } // Navigation property
+
+        //one-to-one Relationship 
+        public StudentProfile? StudentProfile { get; set; }
+
+        //many-to-many relationship 
+        public ICollection<Enrollment>? Enrollments { get; set; }
     }
 }
