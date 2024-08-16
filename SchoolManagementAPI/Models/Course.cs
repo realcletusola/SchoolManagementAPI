dc83c6d1c36ddb55a70cdf80da2744ce0e22105a
cletus <cletushunsu@gmail.com>
2024-08-16 04:36:43 -0700

    created course model, linking course to enrollment model as many-to-many relationship

---
+++ SchoolManagementAPI/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿// course model 
+namespace SchoolManagementAPI.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; } = string.Empty;
+
+        // many-to-many relationship 
+        public ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
